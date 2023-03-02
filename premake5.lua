1c202be49450f2c273223f39d587f16947c25896
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-03-02 23:14:48 +0200

    Add premake and raylib

---
+++ premake5.lua
@@ -0,0 +1,55 @@
+
+workspace "2223-9th-grade-sprint-math-games"
+    configurations { 
+        "Debug", 
+        "Release" 
+    }
+
+    architecture "x64"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+   
+project "2223-9th-grade-sprint-math-games"
+    location "2223-9th-grade-sprint-math-games"
+    kind "ConsoleApp"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/**.h"
+    }
+    
+    includedirs
+    {
+      "vendor/raylib/include",
+      "%{prj.name}/src"
+    }
+
+    libdirs
+    {
+      "vendor/raylib/lib"
+    }
+
+    links
+    {
+      "raylibdll"
+    }
+
+    postbuildcommands
+    {
+        ("{COPY} ../vendor/raylib/lib/raylib.dll ../bin/" .. outputdir .. "/2223-9th-grade-sprint-math-games")
+    }
+    
+    cppdialect "C++20"
+    staticruntime "On"
+    systemversion "latest"
+    
+    filter "configurations:Debug"
+      symbols "On"
+    
+    filter "configurations:Release"
+      optimize "On"

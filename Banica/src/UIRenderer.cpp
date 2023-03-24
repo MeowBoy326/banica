0a531aa1a6f790f1fb0703c4d15911f262e8f008
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-03-24 21:53:49 +0200

    Center result

--- Banica/src/UIRenderer.cpp
+++ Banica/src/UIRenderer.cpp
@@ -27,11 +27,11 @@ namespace bnc
 
     void UIRenderer::RenderResult()
     {
-        Vector2 position = {GetScreenWidth() - (m_UIData->p_Result->size() * 60) - 100.0f, GetScreenHeight() - 200.0f};
+        Vector2 position = {GetScreenWidth() - (m_UIData->p_Result->size() * 60) - 100.0f, (GetScreenHeight() / 2)};
 
         DrawTextEx(m_UIData->mainFont, "RESULT",
          Vector2({position.x + 60, position.y - 60}), 
-         36, 2, BLACK);
+         36, 2, RAYWHITE);
 
         for(size_t i = 0; i < m_UIData->p_Result->size(); i++)
         {
